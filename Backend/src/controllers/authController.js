d90c23d7e78ffa3434e8f347295ba8644e92c8e4
Bhumi <bhumijaswani28@gmail.com>
2026-09-15 12:26:05 +0530

    Fixing cookie in login fucntion

--- Backend/src/controllers/authController.js
+++ Backend/src/controllers/authController.js
@@ -41,7 +41,8 @@ export const login = async (req, res) => {
 
     res.cookie('token', token, {
       httpOnly: true,
-      sameSite: 'lax',
+      sameSite: 'none',
+      secure: true,    
       maxAge: 24 * 60 * 60 * 1000, // 1 day
     });
 
