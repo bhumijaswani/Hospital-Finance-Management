24bb63bc989e3aad609e2f6661304dead9b46b16
Bhumi <bhumijaswani28@gmail.com>
2026-09-16 00:53:26 +0530

    fixed authController.js response

--- Backend/src/controllers/authController.js
+++ Backend/src/controllers/authController.js
@@ -45,12 +45,12 @@ export const login = async (req, res) => {
       secure: true,    
       maxAge: 24 * 60 * 60 * 1000, // 1 day
     });
-
-    res.status(200).json({
-      message: 'Login successful',
-      user: { id: user.id, name: user.name, email: user.email, role: user.role, department: user.department },
-    }); 
-  } 
+  res.status(200).json({
+  message: 'Login successful',
+  token,
+  user: { id: user.id, name: user.name, email: user.email, role: user.role, department: user.department },
+});
+}
   catch (err) {
     console.error(err);
     res.status(500).json({ message: 'Server error during login' });
