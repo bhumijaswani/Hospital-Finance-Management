f8c12180279977ab55324c20ca5d81933b905abd
Bhumi <bhumijaswani28@gmail.com>
2026-09-15 12:23:54 +0530

    Fixing baseUrl

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://hospital-finance-management.onrender.com',
   withCredentials: true, // sabhi requests pe cookie automatically bhejega
 });
